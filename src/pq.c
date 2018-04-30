37078c166a252c86e70527cc6e8aa143ad64e448
Ilya Uvarenkov <trukanduk@gmail.com>
2018-04-30 03:40:09 +0300

    pq in C fix + nn.sh -> run.sh

--- src/pq.c
+++ src/pq.c
@@ -16,7 +16,8 @@ typedef struct _config {
     int num_bits_per_code;
     int num_threads;
     int use_opq;
-    int compute_dist;
+    int compute_error;
+    int kmeans_iterations;
 
     long long num_vectors;
     int num_dimensions;
@@ -26,6 +27,14 @@ typedef struct _config {
     char* output_centroids;
     char* output_error;
 } config_t;
+
+typedef struct _centroids_codebook {
+    int num_clusters;
+    int num_dimensions;
+    int num_parts;
+    float* centroids_pool;
+    float** centroids;
+} centroids_codebook_t;
 
 enum {
     LOAD_BATCH_SIZE = 128 * 1024
@@ -72,11 +81,11 @@ static void load_vectors_dimensions(const char* input_filename, long long num_ve
     long long got_vectors = 0LL;
     while (got_vectors < num_vectors) {
         long long current_batch_size = minll(num_vectors - got_vectors, LOAD_BATCH_SIZE);
-        long long red_rows = fread(batch, current_batch_size, file_row_size, f);
+        long long red_rows = fread(batch, file_row_size, current_batch_size, f);
         for (long long row_index = 0; row_index < red_rows; ++row_index) {
             memcpy(output + (got_vectors + row_index) * num_dimensions,
-                   batch + (file_num_dimensions + 1) * row_index,
-                   num_dimensions);
+                   batch + (file_num_dimensions + 1) * row_index + from_dimension + 1,
+                   num_dimensions * sizeof(float));
         }
         got_vectors += red_rows;
     }
@@ -85,10 +94,49 @@ static void load_vectors_dimensions(const char* input_filename, long long num_ve
     fclose(f);
 }
 
+static double compute_error(const char* input_filename, long long num_vectors,
+                            const unsigned char* indices, const centroids_codebook_t* codebook) {
+    FILE* f = fopen(input_filename, "rb");
+    int file_num_dimensions;
+    fread(&file_num_dimensions, 1, sizeof(file_num_dimensions), f);
+    fseek(f, 0, SEEK_SET);
+    long long file_row_size = file_num_dimensions * sizeof(float) + sizeof(int);
+    float* batch = malloc(file_row_size * LOAD_BATCH_SIZE);
+    long long got_vectors = 0LL;
+    double sum_error = 0.0;
+    while (got_vectors < num_vectors) {
+        long long current_batch_size = minll(num_vectors - got_vectors, LOAD_BATCH_SIZE);
+        long long red_rows = fread(batch, file_row_size, current_batch_size, f);
+
+        for (long long row_index = 0; row_index < red_rows; ++row_index) {
+            const unsigned char* vec_indices =
+                    indices + codebook->num_parts * (got_vectors + row_index);
+            const float* vec_start = batch + (file_num_dimensions + 1) * row_index + 1;
+            for (int part_index = 0; part_index < codebook->num_parts; ++part_index) {
+                const float* centroid = codebook->centroids[part_index] +
+                        codebook->num_dimensions * vec_indices[part_index];
+                for (int dim_index = 0; dim_index < codebook->num_dimensions; ++dim_index) {
+                    float vec_v = vec_start[codebook->num_dimensions * part_index + dim_index];
+                    float cent_v = centroid[dim_index];
+                    float delta = vec_v - cent_v;
+                    sum_error += 1.0 * delta * delta;
+                }
+            }
+        }
+
+        got_vectors += red_rows;
+    }
+    free(batch);
+    batch = NULL;
+    fclose(f);
+
+    return sum_error / num_vectors;
+}
+
 static void print_help(const char* argv0) {
     fprintf(stderr,
             "Usage: ./%s <input file> <output template> <m> "
-            "[--num-threads <nt>] [--compute-dist]\n",
+            "[--num-threads <nt>] [--compute-error]\n",
             argv0);
     exit(1);
 }
@@ -106,10 +154,11 @@ static void parse_args(config_t* config, int argc, const char* argv[]) {
     config->input_filename = argv[1];
     config->output_template = argv[2];
     config->m = atoi(argv[3]);
-    config->compute_dist = 0;
+    config->compute_error = 0;
     config->num_bits_per_code = 8; // TODO:
     config->num_threads = 1; // TODO:
     config->use_opq = 0; // TODO:
+    config->kmeans_iterations = 10;
     if (config->m <= 0) {
         fprintf(stderr, "Invalid m: %d\n", config->m);
         print_help(argv[0]);
@@ -118,8 +167,10 @@ static void parse_args(config_t* config, int argc, const char* argv[]) {
     for (int arg_index = 4; arg_index < argc; ++arg_index) {
         if (!strcmp(argv[arg_index], "--num-threads")) {
             config->num_threads = atoi(argv[++arg_index]);
-        } else if (!strcmp(argv[arg_index], "--compute-dist")) {
-            config->compute_dist = 1;
+        } else if (!strcmp(argv[arg_index], "--compute-error")) {
+            config->compute_error = 1;
+        } else if (!strcmp(argv[arg_index], "--kmeans-iterations")) {
+            config->kmeans_iterations = atoi(argv[++arg_index]);
         } else {
             fprintf(stderr, "Unknown argument: %s\n", argv[arg_index]);
             show_help = 1;
@@ -153,25 +204,19 @@ static void config_free(config_t* config) {
     }
 }
 
-static void plus(float* dists_sum, const float* dists, long long num_vectors) {
-    if (!dists_sum) {
-        return;
-    }
-
-    float* sum_it = dists_sum;
-    const float* dist_it = dists;
-    for (; sum_it != dists_sum + num_vectors; ++sum_it, ++dist_it) {
-        *sum_it += *dist_it;
-    }
-}
-
-static void copy_cluster_indices(unsigned char* result, const int* clusters,
+static void copy_cluster_indices(unsigned char* result, int offset, const int* clusters,
                                  long long num_vectors, int m) {
+#if 0
     unsigned char* res_it = result;
     const int* clusters_it = clusters;
     for (; clusters_it != clusters + num_vectors; res_it += m, ++clusters_it) {
         *res_it = *clusters_it;
     }
+#else
+    for (long long i = 0; i < num_vectors; ++i) {
+        result[i * m + offset] = (unsigned char) clusters[i];
+    }
+#endif
 }
 
 static void save_indices(const char* output_filename, const unsigned char* result,
@@ -192,19 +237,42 @@ static void save_error(const char* output_filename, const float* dists_sum, long
     fclose(f);
 }
 
+// TODO: Move to library part
+static void centroids_codebook_init(centroids_codebook_t* codebook, int num_parts,
+                                    int num_clusters, int num_dimensions) {
+    codebook->num_parts = num_parts;
+    codebook->num_clusters = num_clusters;
+    codebook->num_dimensions = num_dimensions;
+    codebook->centroids_pool = malloc(sizeof(*codebook->centroids_pool) * num_parts
+                                      * num_clusters * num_dimensions);
+    codebook->centroids = malloc(sizeof(*codebook->centroids) * num_parts);
+    for (int part_index = 0; part_index < num_parts; ++part_index) {
+        codebook->centroids[part_index] = codebook->centroids_pool
+                + part_index * num_clusters * num_dimensions;
+    }
+}
+
+static void centroids_codebook_destroy(centroids_codebook_t* codebook) {
+    free(codebook->centroids_pool);
+    codebook->centroids_pool = NULL;
+    free(codebook->centroids);
+    codebook->centroids = NULL;
+}
+
+static void centroids_codebook_save(const centroids_codebook_t* codebook, const char* filename) {
+    FILE* f = fopen(filename, "wb");
+    fwrite(codebook->centroids_pool, sizeof(*codebook->centroids_pool) * codebook->num_dimensions,
+           codebook->num_parts * codebook->num_clusters, f);
+    fclose(f);
+}
+
 static void run(const config_t* config) {
     float* data = malloc(sizeof(*data) * config->num_vectors * config->num_dimensions_per_part);
-    unsigned char* result = malloc(sizeof(*result) * config->num_vectors * config->m);
+    unsigned char* result = calloc(sizeof(*result) * config->num_vectors * config->m, 1);
     int* clusters = malloc(sizeof(*clusters) * config->num_vectors);
-    float* centroids =
-            malloc(sizeof(*centroids) * config->num_clusters * config->num_dimensions_per_part);
-    float* dists = NULL;
-    float* dists_sum = NULL;
-    if (config->compute_dist) {
-        dists = malloc(sizeof(*dists) * config->num_vectors);
-        dists_sum = malloc(sizeof(*dists_sum) * config->num_vectors);
-    }
-    FILE* centroids_file = fopen(config->output_centroids, "wb");
+    centroids_codebook_t codebook;
+    centroids_codebook_init(&codebook, config->m, config->num_clusters,
+                            config->num_dimensions_per_part);
 
     for (int part_index = 0; part_index < config->m; ++part_index) {
         printf("Starting part %d\n", part_index);
@@ -212,30 +280,33 @@ static void run(const config_t* config) {
                                 config->num_dimensions_per_part * part_index,
                                 config->num_dimensions_per_part, data);
         kmeans(config->num_dimensions_per_part, config->num_vectors, config->num_clusters,
-               -1, data, config->num_threads | KMEANS_INIT_BERKELEY, time(NULL), 1, centroids,
-               dists, clusters, NULL);
-        copy_cluster_indices(result + part_index, clusters, config->num_vectors, config->m);
-        fwrite(centroids, sizeof(*centroids),
-               config->num_clusters * config->num_dimensions_per_part, centroids_file);
-        if (config->compute_dist) {
-            plus(dists_sum, dists, config->num_vectors);
-        }
+               100, data, config->num_threads | KMEANS_INIT_BERKELEY,
+               time(NULL), 1, codebook.centroids[part_index], NULL, clusters, NULL);
+        copy_cluster_indices(result, part_index, clusters, config->num_vectors, config->m);
     }
-    fclose(centroids_file);
-    centroids_file = NULL;
 
+    // for (int part_index = 0; part_index < config->m; ++part_index) {
+    //     printf("PART %d\n", part_index);
+    //     for (int index = 0; index < config->num_clusters; ++index) {
+    //         for (int d = 0; d < config->num_dimensions_per_part; ++d) {
+    //             printf("%f ", codebook.centroids[part_index][index * config->num_dimensions_per_part + d]);
+    //         }
+    //         printf("\n");
+    //     }
+    //     printf("\n\n");
+    // }
+
+    centroids_codebook_save(&codebook, config->output_centroids);
     save_indices(config->output_vectors, result, config->num_vectors, config->m);
 
-    if (config->compute_dist) {
-        save_error(config->output_error, dists_sum, config->num_vectors);
-
-        free(dists);
-        dists = NULL;
-        free(dists_sum);
-        dists_sum = NULL;
+    if (config->compute_error) {
+        double error = compute_error(config->input_filename, config->num_vectors, result,
+                                     &codebook);
+        FILE* f = fopen(config->output_error, "a");
+        fprintf(f, "%lf\n", error);
+        fclose(f);
     }
-    free(centroids);
-    centroids = NULL;
+    centroids_codebook_destroy(&codebook);
     free(clusters);
     clusters = NULL;
     free(result);
--- src/run.sh
+++ src/run.sh
@@ -3,8 +3,9 @@
 PQ_HOME=${PQ_HOME:-$HOME/pq}
 
 print_help() {
-    echo "Usage NUM_NN=100 NUM_THREADS=4 ./$0 exact <dataset>" >&2
-    echo "      $0 fast <dataset> <num-dimensions> <num-blocks> <num-splits> <num-threads>" >&2
+    echo "Usage NUM_NN=100 NUM_THREADS=4 ./$0 nn-exact <dataset>" >&2
+    echo "      NUM_NN=100 NUM_THREADS=4 NUM_DIM=3 NUM_SPLITS=10 NUM_BLOSKS=11 ./$0 nn-fast <dataset>" >&2
+    echo "      M=8 NUM_THREADS=4 ./$0 pq <dataset>" >&2
     exit 1
 }
 
@@ -24,13 +25,14 @@ diff_ts() {
     echo $1 | python3 -c 'import time; print(time.time() - float(input()))'
 }
 
-nn_type=$1
+action=$1
 dataset=$2
 
-NUM_NN=${NUM_NN:-50}
-NUM_THREADS=${NUM_THREADS:-1}
-if [ "$nn_type" = 'exact' ]
+if [ "$action" = 'nn-exact' ]
 then
+    NUM_NN=${NUM_NN:-50}
+    NUM_THREADS=${NUM_THREADS:-1}
+
     OUT_DIR="$PQ_HOME/out/nn/${dataset}_${NUM_NN}"
     mkdir -p $OUT_DIR
 
@@ -43,9 +45,12 @@ then
 
     echo "    Done in $(diff_iso $start)"
 
-elif [ "$nn_type" = 'fast' ]
+elif [ "$action" = 'nn-fast' ]
 then
     make compute_nn_fast || exit 1
+
+    NUM_NN=${NUM_NN:-50}
+    NUM_THREADS=${NUM_THREADS:-1}
 
     NUM_DIM=${NUM_DIM:-3}
     NUM_SPLITS=${NUM_SPLITS:-10}
@@ -61,14 +66,33 @@ then
             --with-blocks-stat --num-threads $NUM_THREADS --num-dims $NUM_DIM \
             --num-splits-per-dim $NUM_SPLITS --num-blocks-per-dim $NUM_BLOCKS > "$OUT_DIR/stdout.log" 2> "$OUT_DIR/stderr.log" || exit 1
     t=$(diff_ts $start)
-    echo $t > "$OUT_DIR/time_${NUM_THREADS}"
+    echo $t >> "$OUT_DIR/time_${NUM_THREADS}"
 
     echo "    Done nn in $(diff_iso $start). Starting compare..."
     EXACT_OUT="$PQ_HOME/out/nn/${dataset}_${NUM_NN}"
-    python3 compare_nn.py "$EXACT_OUT/" "$OUT_DIR/" > $OUT_DIR/diff.json
+    python3 compare_nn.py "$EXACT_OUT/" "$OUT_DIR/" >> $OUT_DIR/diff.json
+    echo "    Done in $(diff_iso $start)"
+
+elif [ "$action" = 'pq' ]
+then
+    make pq || exit 1
+
+    M=${M:-8}
+    NUM_THREADS=${NUM_THREADS:-1}
+
+    OUT_DIR="$PQ_HOME/out/pq/${dataset}_${M}"
+    mkdir -p $OUT_DIR
+
+    echo "Starting fast $dataset with M=$M, NUM_THREADS=$NUM_THREADS at $(now_iso)..."
+
+    start=$(now_ts)
+    ./pq "$PQ_HOME/data/${dataset}.fvecs" "$OUT_DIR/" $M \
+            --num-threads $NUM_THREADS --compute-error # > "$OUT_DIR/stdout.log" 2> "$OUT_DIR/stderr.log" || exit 1
+    t=$(diff_ts $start)
+    echo $t >> "$OUT_DIR/time_${NUM_THREADS}"
     echo "    Done in $(diff_iso $start)"
 
 else
-    echo "Unknown NN type: '$nn_type'" >&2
+    echo "Unknown action: '$action'" >&2
     print_help
 fi
